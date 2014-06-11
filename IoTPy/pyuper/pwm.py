52397a947b69674fe818add00199de1e2d118101
Giedrius <giedrius@8devices.com>
2014-06-11 16:37:04 +0300

    PWM module bugfix and clarification.
    1) PWM write function used incorrect (constant) period value to
    calculate active time.
    2) Clarified internal data structure by changing it from list to dict.

--- IoTPy/pyuper/pwm.py
+++ IoTPy/pyuper/pwm.py
@@ -2,7 +2,8 @@ from IoTPy.pyuper.pinouts import CAP_PWM
 from IoTPy.pyuper.utils import IoTPy_APIError, errmsg
 
 
-PWM_PORT_RUNNING = [[0,0], [0,0]]
+PWM_PORT_RUNNING = [{'channels':0, 'period':0}, {'channels':0, 'period':0}]
+
 class PWM:
     """
     PWM (Pulse Width Modulation) pin module.
@@ -16,7 +17,6 @@ class PWM:
     """
 
     _PWM_PORT_FUNCTIONS = [[50,51,52],[60,61,62]]
-    _PWM_PERIOD = 10000
     _PWM_PORT_MAX = [0xffff, 0xffffffff]      #maxuint16, maxuint32
 
     def __init__(self, board, pin, polarity = 1):
@@ -31,9 +31,9 @@ class PWM:
         self.primary = True
         self.hightime = 0
         self.polarity = polarity
-        PWM_PORT_RUNNING[self.pwm_port][0] += 1
-        if PWM_PORT_RUNNING[self.pwm_port][0] == 1:
-            self.period(self._PWM_PERIOD)
+        PWM_PORT_RUNNING[self.pwm_port]['channels'] += 1
+        if PWM_PORT_RUNNING[self.pwm_port]['channels'] == 1:
+            self.period(10000)
 
     def period(self, period):
         """
@@ -44,9 +44,9 @@ class PWM:
         :raise: IoTPy_APIError
         """
         if 0 <= period <= self._PWM_PORT_MAX[self.pwm_port]:
-            if PWM_PORT_RUNNING[self.pwm_port][1] != period:
+            if PWM_PORT_RUNNING[self.pwm_port]['period'] != period:
                 self.board.uper_io(0, self.board.encode_sfp(self._PWM_PORT_FUNCTIONS[self.pwm_port][0], [period]))
-                PWM_PORT_RUNNING[self.pwm_port][1] = period
+                PWM_PORT_RUNNING[self.pwm_port]['period'] = period
         else:
             errmsg("UPER API: PWM period for port %d can be only between 0-%d" % (self.pwm_port, self._PWM_PORT_MAX[self.pwm_port]))
             raise IoTPy_APIError("PWM period is out of range.")
@@ -62,10 +62,10 @@ class PWM:
         if self.primary:
             self.board.uper_io(0, self.board.encode_sfp(2, [self.logical_pin])) # set pin secondary function
             self.primary = False
-        if 0 <= hightime <= PWM_PORT_RUNNING[self.pwm_port][1]:
+        if 0 <= hightime <= PWM_PORT_RUNNING[self.pwm_port]['period']:
             self.hightime = hightime
             if self.polarity == 1:
-                hightime = PWM_PORT_RUNNING[self.pwm_port][1] - hightime
+                hightime = PWM_PORT_RUNNING[self.pwm_port]['period'] - hightime
             self.board.uper_io(0, self.board.encode_sfp(PWM._PWM_PORT_FUNCTIONS[self.pwm_port][1], [self.pwm_pin, hightime]))
         else:
             errmsg("UPER error: PWM high time is out of range on logical pin %d." % self.logical_pin)
@@ -78,7 +78,7 @@ class PWM:
         :param duty: PWM duty cycle in fractions: 1.0 is 100%, 0.5 is 50%, etc.
         :type duty: float
         """
-        self.width_us(int((self._PWM_PERIOD)*float(duty)))
+        self.width_us(int(PWM_PORT_RUNNING[self.pwm_port]['period']*float(duty)))
 
     def read(self):
         """
@@ -86,14 +86,14 @@ class PWM:
 
         :return: PWM duty cycle.
         """
-        return float(self.hightime) / PWM_PORT_RUNNING[self.pwm_port][1]
+        return float(self.hightime) / PWM_PORT_RUNNING[self.pwm_port]['period']
 
     def __exit__(self, exc_type, exc_value, traceback):
-        PWM_PORT_RUNNING[self.pwm_port][0] -= 1
+        PWM_PORT_RUNNING[self.pwm_port]['channels'] -= 1
         self.primary = True
         self.board.uper_io(0, self.board.encode_sfp(1, [self.logical_pin])) # set pin primary function
-        if PWM_PORT_RUNNING[self.pwm_port][0] == 0:
-            PWM_PORT_RUNNING[self.pwm_port][1] = 0
+        if PWM_PORT_RUNNING[self.pwm_port]['channels'] == 0:
+            PWM_PORT_RUNNING[self.pwm_port]['period'] = 0
             self.board.uper_io(0, self.board.encode_sfp(PWM._PWM_PORT_FUNCTIONS[self.pwm_port][2], [self.pwm_pin]))
 
     def __enter__(self):
